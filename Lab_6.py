6b788755d591d2b302674d44149d7b040d637ce7
Denis_Vdovin <128417998+Kolavorot@users.noreply.github.com>
2023-05-20 11:12:48 +0400

    Update Lab_6.py

--- Lab_6.py
+++ Lab_6.py
@@ -3,10 +3,12 @@
 #(от каждой партии должно быть от 1 до 3 членов).
 
 import itertools
-K = int(input('Введите колличество членов коммисии '))
+K = int(input('Введите колличество членов коммисии ')) #Ввод данных
 N = int(input('Введите колличество партий '))
-combinations = set(itertools.combinations_with_replacement(range(N + 1), K))
-unique_combinations = set(map(tuple, map(sorted, combinations)))
-for combination in unique_combinations:
+if K < N:
+    print("Ошибка ввода")
+combinations = set(itertools.combinations_with_replacement(range(N + 1), K)) #Создаём все возможные комбинации
+unique_combinations = set(map(tuple, map(sorted, combinations))) #Убираем повторения
+for combination in unique_combinations:                    #Добавляем условие от 1 до 3 кандидатов из каждой партии
     if combination.count(0) <= 3 and combination.count(1) <= 3 and combination.count(2) <= 3:
         print(combination)
