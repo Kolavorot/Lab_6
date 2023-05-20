b4a2bd8aae5de82e545759a84de374e256080ddb
Denis_Vdovin <128417998+Kolavorot@users.noreply.github.com>
2023-05-20 11:04:22 +0400

    Update Lab_6.py

--- Lab_6.py
+++ Lab_6.py
@@ -1,1 +1,8 @@
-
+import itertools
+K = int(input('Введите колличество членов коммисии '))
+N = int(input('Введите колличество партий '))
+combinations = set(itertools.combinations_with_replacement(range(N + 1), K))
+unique_combinations = set(map(tuple, map(sorted, combinations)))
+for combination in unique_combinations:
+    if combination.count(0) <= 3 and combination.count(1) <= 3 and combination.count(2) <= 3:
+        print(combination)
